5d6e3dc78cab327129072decdec109bf49b1358e
abadat <abadat26@gmail.com>
2023-05-03 01:07:27 +0600

    afterall vercel install successfully

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 const port = 5000
+const recipes = require('./data/data.json')
+
+app.use(cors())
 
 app.get('/', (req, res)=>{
-    res.send('Hello world')
+    res.send('Hello! server running')
 })
 
+app.get('/recipes', (req, res)=>{
+    res.send(recipes)
+
+})
 
 
 
